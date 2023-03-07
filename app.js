77f0e8d962ff651eb8517c71fdd45f29c083bce6
Vitaliy Gerassimov <gerassimov.developer@gmail.com>
2023-03-07 11:10:07 +0600

    14

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-// const routerAuth = require("./routes/api/auth");
-// const routerUsers = require("./routes/api/users");
-const routerContacts = require("./routes/api/contacts");
+
 const routerApi = require("./routes/api");
 
 const auth = require("./middlewaries/auth");
@@ -16,9 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", routerContacts);
-// app.use("/api/auth", routerAuth);
-// app.use("/api/users", auth, routerUsers);
+app.use("/api/contacts", routerApi.contacts);
 app.use("/api/auth", routerApi.auth);
 app.use("/api/users", auth, routerApi.users);
 
